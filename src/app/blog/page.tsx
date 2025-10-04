bdea2a338106713a45b6fa7540ea3e284d4d0b8f
jayesh-0104 <jayeshmistry.testriq@gmail.com>
2025-10-04 19:44:52 +0530

    blog post updated into sections

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,29 +1,17 @@
 import dynamic from 'next/dynamic';
 import React from 'react'
 
-// const SampleBlogPost = dynamic(
-//     () => import("@/components/Sections/SampleBlogPost"),
-//     {
-//         ssr: true,
-//         loading: () => (
-//             <div className="flex items-center justify-center h-screen bg-[theme(color.background)]">
-//                 <p className="text-gray-500">Loading...</p>
-//             </div>
-//         ),
-//     }
-// );
-
-// const BlogPage = dynamic(
-//     () => import("@/components/Sections/BlogPage"),
-//     {
-//         ssr: true,
-//         loading: () => (
-//             <div className="flex items-center justify-center h-screen bg-[theme(color.background)]">
-//                 <p className="text-gray-500">Loading...</p>
-//             </div>
-//         ),
-//     }
-// );
+const BlogPage = dynamic(
+    () => import("@/components/Sections/BlogPage"),
+    {
+        ssr: true,
+        loading: () => (
+            <div className="flex items-center justify-center h-screen bg-[theme(color.background)]">
+                <p className="text-gray-500">Loading...</p>
+            </div>
+        ),
+    }
+);
 
 const BlogHeroSection = dynamic(
     () => import("@/components/Sections/BlogHeroSection"),
@@ -76,9 +64,7 @@ const BlogGridSection = dynamic(
 export default function page() {
     return (
         <>
-            {/* <SampleBlogPost /> */}
-            {/* <BlogPage /> */}
-
+            
             <div className='bg-gray-100'>
                 <BlogHeroSection />
                 <BlogFeaturedSection />
