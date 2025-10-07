c252905c8ae9b19357daa8782f3ea7a048ee7b30
Prakash Mishra <>
2025-10-07 13:05:51 +0530

    changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,15 +2,8 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-<<<<<<< HEAD
 const BlogPageNewUI = dynamic(
     () => import("@/components/Sections/BlogPageNewUI"),
-=======
-
-
-const BlogHeroSection = dynamic(
-    () => import("@/components/Sections/BlogHeroSection"),
->>>>>>> main
     {
         ssr: true,
         loading: () => (
@@ -24,17 +17,7 @@ const BlogHeroSection = dynamic(
 export default function page() {
     return (
         <>
-<<<<<<< HEAD
             <BlogPageNewUI />
-=======
-            
-            <div className='bg-gray-100'>
-                <BlogHeroSection />
-                <BlogFeaturedSection />
-                <BlogCategoriesSection />
-                <BlogGridSection />
-            </div>
->>>>>>> main
         </>
     );
 }
