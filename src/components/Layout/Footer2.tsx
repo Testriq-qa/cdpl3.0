90c1b79c27e2bdf337efac83a3c86dd5c5052511
jayesh-0104 <jayeshmistry.testriq@gmail.com>
2025-10-09 11:18:31 +0530

    Linked footer links to 4 page 1. privacy-policy 2. terms-of-service 3. cookies-policy 4. cancellation-refund-policy

--- src/components/Layout/Footer2.tsx
+++ src/components/Layout/Footer2.tsx
@@ -50,9 +50,9 @@ const Footer: React.FC = () => {
               <span className='hidden lg:block'>|</span>
               <Link href="/cookies-policy" className="hover:text-orange-400 transition-colors md:mr-0 lg:mr-4">Cookies Policy</Link>
               <span className='hidden lg:block'>|</span>
-              <Link href="/terms-conditions" className="hover:text-orange-400 transition-colors md:mr-3 lg:mr-0 xl:mr-4">Terms and Conditions</Link>
+              <Link href="/terms-of-service" className="hover:text-orange-400 transition-colors md:mr-3 lg:mr-0 xl:mr-4">Terms and Conditions</Link>
               <span className='hidden xl:block'>|</span>
-              <Link href="/cancellation-refund" className="hover:text-orange-400 transition-colors">Cancellation/Refund Policy</Link>
+              <Link href="/cancellation-refund-policy" className="hover:text-orange-400 transition-colors">Cancellation/Refund Policy</Link>
             </div>
             <p className="mt-6 text-sm text-center md:text-right">ISO 9001:2015 (QMS) 27001:2013 (ISMS) Certified Company.</p>
           </div>
